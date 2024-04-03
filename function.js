d2c1e1f1b4463b8a3a267463db36a601f69044e3
quoccuong2005 <161569929+quoccuong2005@users.noreply.github.com>
2024-04-03 21:39:44 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,23 @@
+//function tinhtong(a, b) {
+    //return a + b;
+//}
+//console.log(tinhtong(2, 3));
+
+
+
+function tinhtong(a, b) {
+    return 0;
+}
+console.log(tinhtong(2, 3));
+
+function tinhgiaithua(n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * tinhgiaithua(n - 1);
+}
+console.log(tinhgiaithua(5));
+
+
+
+
---
+++ thuchanh.js
@@ -0,0 +1,19 @@
+var array1=[1,2,3,4,5];
+var array2=[6,7,8,9,10];
+var array3=[4,2,5,1,3];
+array1.length;
+console.log(array1.length); 
+console.log(array1.join( '|' ));
+console.log(array1.concat(array2));
+console.log(array1.slice(1,3));
+console.log(array1.reverse());
+console.log(array3.sort());
+array1.push(6,7);
+console.log(array1)
+array1.pop()
+console.log(array1);
+array2.unshift(4,5);
+console.log(array2);
+array3.shift();
+console.log(array3);
+
